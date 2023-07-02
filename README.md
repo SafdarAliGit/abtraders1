cb38ba169eb8d78e3f6c57c23fc32d26aa60c253
SafdarAliGit <safdar211@gmail.com>
2023-07-02 09:21:39 +0500

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Abtraders1
+
+this is business application
+
+#### License
+
+Tech Ventures
---
+++ abtraders1/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Abtraders1",
+			"type": "module",
+			"label": _("Abtraders1")
+		}
+	]
